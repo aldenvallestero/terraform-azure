0ae7d5870edc4c3e04ea53673e97ad4cd63cf5d9
Alden Vallestero <alden.v@agtuary.com>
2024-01-04 10:58:57 +0800

    🧱 Provision Network Security Group & Rule

--- main.tf
+++ main.tf
@@ -50,3 +50,26 @@ resource "azurerm_subnet" "dev-playground-subnet" {
   virtual_network_name = azurerm_virtual_network.dev-playground-net.name
   address_prefixes = [ "10.0.2.0/24" ]
 }
+
+resource "azurerm_network_security_group" "dev-playground-net-sg" {
+  name = "dev-playground-net-sg"
+  location = azurerm_resource_group.dev-playground-rg.location
+  resource_group_name = azurerm_resource_group.dev-playground-rg.name
+  tags = {
+    env = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "dev-playground-net-sr" {
+  name = "dev-playground-net-sr"
+  priority = 100
+  direction = "Inbound"
+  access = "Allow"
+  protocol = "*"
+  source_port_range = "*"
+  destination_port_range = "*"
+  source_address_prefix = "*"
+  destination_address_prefix = "*"
+  resource_group_name = azurerm_resource_group.dev-playground-rg.name
+  network_security_group_name = azurerm_network_security_group.dev-playground-net-sg.name
+}
